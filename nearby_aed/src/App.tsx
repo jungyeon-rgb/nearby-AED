444b41982fc9dc899f4bdbe778d5870872bb9e21
김정연 <jy.keem2468@gmail.com>
2023-06-19 22:10:41 +0900

    초기세팅(react,ts,vite 설치)

---
+++ nearby_aed/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1> 내 주위 제세동기입니다 ♥️</h1>
+    </>
+  );
+}
+
+export default App;
