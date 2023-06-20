13151b7ee397f410ce3a89cacc2528935a49f7c4
김정연 <jy.keem2468@gmail.com>
2023-06-20 16:01:40 +0900

    Geolocation 컴포넌트에 카카오 api 추가 및 코드 작성

--- nearby_aed/src/components/Geolocation.tsx
+++ nearby_aed/src/components/Geolocation.tsx
@@ -1,3 +1,32 @@
+import axios from "axios";
+
+const onGeoOk = (position: GeolocationPosition) => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  // Kakao REST API에 get 요청을 보냅니다.
+  // 파라미터 x, y에 lon, lat을 넣어주고 API_KEY를 Authorization 헤더에 넣어줍니다.
+  axios
+    .get(
+      `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lon}&y=${lat}&input_coord=WGS84`,
+      {
+        headers: { Authorization: `KakaoAK ${process.env.REACT_APP_REST_API}` },
+      }
+    )
+    .then((res) => {
+      console.log(res.data.documents);
+      dispatch(changeRegion(res.data.documents[0].address.region_1depth_name));
+      dispatch(changeCity(res.data.documents[0].address.region_2depth_name));
+    })
+    .catch((e) => console.log(e));
+};
+
+const onGeoError = () => {
+  alert("위치권한을 확인해주세요");
+};
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
 export const Geolocation = () => {
   return "현재위치컴포넌트입니다";
 };
---
+++ nearby_aed/src/components/VoiceOver.tsx
@@ -0,0 +1,7 @@
+export const VoiceOver = () => {
+  return "voice over 기능입니다. ";
+};
+
+// 모바일 접근성은 (MOBILE APP 접근성 Mobile APP Accessibility)은
+// 「지능정보화기본법」에 따라 모바일 애플리케이션 서비스 제공자가 장애인과 고령자 분들이
+// 비장애인과 동등하게 모바일 애플리케이션 콘텐츠에 접근할 수 있도록 하는 것
